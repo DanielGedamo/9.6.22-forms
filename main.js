8e804b0f1320a6948170f61ee8b6d9ee06efff98
DANIELGEDAMO <danielgadamo@gmail.com>
2022-06-15 09:15:01 +0300

    1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function sendtowindow(){
-if(checkFirstName() && checkB_day()){
-return true;
-}
-return false;
+function sendToWindow() {
+  if (checkFirstName() && checkBday()) {
+    return true;
+  }
+  return false;
 }
 // function checkFirstName(){
 //     // // if(userName.value [0] == userName.value [0].toUooerCase()){
@@ -15,22 +15,20 @@ return false;
 //     }
 //        alert('no');
 //     return false;
-               
-//     } 
-    // userName_label.innerHTML ="<b>The first letter must be a capital letter!</b>"
-    // userName_label.style.color ="red"
-    // return false
-    
-  
 
-function checkB_day(){
-  var nowyear = new Date();
-  var yearFromUser = document.getElementById(birthDay.value).substr(0,3);
-  if( nowyear.getFullYear() - yearFromUser >18){
-    alert('yes');
-    return true;
+//     }
+// userName_label.innerHTML ="<b>The first letter must be a capital letter!</b>"
+// userName_label.style.color ="red"
+// return false
+ var nowyear =new Date().getFullYear()
+function checkBday() {
   
+     
+ var userInput = document.getElementById("birthDay").value.slice(0, 4);
+  if (nowyear-userInput < 18) {
+   console.log("yes");
+    return true;
   }
-  alert('no');
+  alert("no");
   return false;
-};
+}
